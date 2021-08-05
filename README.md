1973af78a6834e3303254dfb91bf7d8999364921
Ryukyo <florian.fejer@hotmail.de>
2021-08-05 22:52:42 +0900

    Controller more limited return and required input for create

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# README
+# A Ruby on Rails API
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
+rails db:drop db:create db:migrate rails db:seed
+rails c
+rails s
 
 - Ruby version
 
@@ -17,31 +16,31 @@ Things you may want to cover:
 
 - How to run the test suite
 
-- Services (job queues, cache servers, search engines, etc.)
-
 - Deployment instructions
 
 - API
-  ** Score \*** Create
+** Score
+  
+*** Create
 
 ```
 id: 123456,
 player: "player_name",
 score: integer > 0,
-time: "01.01.21.07.30"
+time: "2021-08-05 07:33:41"
 ```
 
-\*\*\* Get
+*** Get
 Returns the score of the given id
 `api/v1/scores/{id}`
 
 Returns a filtered list of scores
-`api/v1/scores/{id}?before=01.01.21.07.30&after=01.01.21.07.30&player=abc1,abc2`
+`api/v1/scores/{id}?before=01.01.21.07.30&after=01.01.21.07.30&players[abc1]=1&players[abc2]=2&players[abc3]=3`
 
 Returns a players score history
 `api/v1/scores?history=abc1`
 
-\*\*\* Delete
+*** Delete
 `api/v1/scores/{id}`
 
 Deletes the score of the given id
--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,32 +1,35 @@
 class Api::V1::ScoresController < ApplicationController
         def index
             @scores = Score.all 
-            render json: @scores, status: 200
+            render json: @scores, only: [:id, :player, :score, :time], status: 200
         end 
     
         def show
             @score = Score.find(params[:id])
             if @score
-                render json: @score, status: 200
+                render json: @score, only: [:id, :player, :score, :time], status: 200
             else
                 render json: {error: "Score not found"}, status: 404
             end
         end 
     
         def create
-            @score = Score.create(
-                player: params[:player],
-                score: params[:score],
-                time: params[:time]
+            @score = Score.create!(
+                score_params
             )
-            render json: @score, status: 201
+            render json: @score, only: [:player, :score, :time], status: 201
         end 
 
-    
         def destroy
             @scores = Score.all 
             @score = Score.find(params[:id])
             @score.destroy
-            render json: @scores, status: 200
-        end 
+            render json: @scores, only: [:player, :score, :time], status: 200
+        end
+        
+        private
+        def score_params
+            params.require(:player,:score,:time)
+        end
+        
 end
